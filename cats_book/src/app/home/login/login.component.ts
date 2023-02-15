7f48da352d41c249339799de5aa3f523ad324f2a
JayJatoba <jguilhermejatoba@hotmail.com>
2023-02-15 19:35:21 -0300

    2 way databinding

--- cats_book/src/app/home/login/login.component.ts
+++ cats_book/src/app/home/login/login.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  user = '';
+  password = '';
 
+  login() {
+    console.log(this.user);
+    console.log(this.password);
+  }
 }
